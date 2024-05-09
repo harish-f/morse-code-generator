e84d6937e72788bea304ddc0ef8efe7cb4344db6
Harish Ram <harish@Harishs-MacBook-Air.local>
2024-05-09 23:24:16 +0800

    made changes to analysis code

--- 07_analyse_received_sound.py
+++ 07_analyse_received_sound.py
@@ -1,16 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-from scipy.fft import fft
+from scipy.fft import *
 
+# plot a small sample of the audio file
 def plot_sound_period(filename):
     sr, data = wavfile.read(filename)
     
-    # using frames 150 to 300
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude / arbitrary units')
+    plt.title('Sound Waveform')
+    
+    # using frames 150 to 300 to ignore any possible static at the start due to recording issues
     plt.plot([150/sr + x/sr for x in range(150,300)], data[150:300])
     plt.show()
 
-def plot_sound_waveform(filename, start, end):
+# plot a specific portion of the file, with specified start and end timings
+# used to decode the morse code based on high and low pulses
+def plot_sound_envelope(filename, start, end):
     # Read the audio file
     sr, data = wavfile.read(filename)
     
@@ -18,42 +25,38 @@ def plot_sound_waveform(filename, start, end):
     data = data[int(start*44100):int(end*44100)]
     time = [x/sr for x in range(int(start*sr), int(start*sr) + len(data))]
     
-    # Plot the waveform
     plt.plot(time, data)
     plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude')
-    plt.title('Sound Waveform')
+    plt.ylabel('Amplitude / arbitrary units')
+    plt.title('Sound Envelope')
     plt.grid(True)
     plt.show()
 
+
+# plot the fft graph of the audio file
 def plot_fft(filename):
     # Read the audio file
-    sample_rate, data = wavfile.read(filename)
+    sr, data = wavfile.read(filename)
     
     # Compute the FFT
     fft_output = fft(data)
     fft_magnitude = np.abs(fft_output)
-    freqs = np.fft.fftfreq(len(data), 1/sample_rate)
+    freqs = fftfreq(len(fft_magnitude), 1/sr)
     
     # Plot the FFT
     plt.figure(figsize=(12, 6))
-    plt.plot(freqs[:len(data)//15], fft_magnitude[:len(data)//15])  # Plot only positive frequencies
+    plt.plot([x for x in freqs[:len(fft_magnitude)//15]], fft_magnitude[:len(fft_magnitude)//15])  # Plot only positive frequencies
+    print(freqs)
     plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Magnitude')
+    plt.ylabel('Magnitude / arbitrary units')
     plt.title('Fast Fourier Transform (FFT)')
     plt.grid(True)
     plt.show()
 
 # Specify the filename of the sound file
-filename = '06_received_sound.wav'  # Replace 'example.wav' with the filename of your sound file
-# filename = '06_received_sound.wav'  # Replace 'example.wav' with the filename of your sound file
+filename = '02_sound.wav'
+filename = '06_received_sound.wav'
 
-# Zoomed in plot of the frequency
 plot_sound_period(filename)
-
-# Plot the sound waveform
-# Start and end of required waveform to be specified
-plot_sound_waveform(filename, start=7.2, end=12.0)
-
-# Plot the FFT graph
-plot_fft(filename)
+# plot_fft(filename)
+# plot_sound_envelope("06_received_sound.wav", start=3, end=6.5)
